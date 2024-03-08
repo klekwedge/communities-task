4b4df18d8561159e047cdfd5b583517bc294ff1f
Andrey <antonov.andrey.10101@gmail.com>
2024-03-08 18:29:43 +0300

    feat: change avatars colors

--- src/components/GroupItem/GroupItem.tsx
+++ src/components/GroupItem/GroupItem.tsx
@@ -28,10 +28,15 @@ function GroupItem({ group }: { group: Group }) {
     <div className="groups__group group" key={group.id}>
       <img
         className="group__image"
-        src={`https://via.placeholder.com/100/${group.avatar_color || '000000'}/FFFFFF/?text=${group.name}`}
+        // * Закоментировать строчку ниже в случае, если очень яркие аватарки сообществ на экране
+        src={`https://placehold.co/100x100/${group.avatar_color || '000000'}/FFFFFF?text=${group.name}&font=roboto`}
+
+        // * Расскоментировать строчку ниже в случае, если очень яркие аватарки сообществ на экране
+        // src={`https://placehold.co/100x100/0077FF/FFFFFF?text=${group.name}&font=roboto`}
         alt={group.name}
       />
       <div>
+        {group.avatar_color}
         <h2 className="group__name">{group.name}</h2>
         <p>{group.closed ? 'Закрытая' : 'Открытая'} группа</p>
 
@@ -51,16 +56,14 @@ function GroupItem({ group }: { group: Group }) {
         </div>
 
         {selectedFriends.length > 0 && (
-          <div className="group__friends">
-            <ul>
-              {selectedFriends.map((friend, index) => (
-                // поскольку элементы массива остаются неизменными, и порядок элементов в массиве сохраняется,
-                // то в таком случае использование индекса массива в качестве ключа является безопасным
-                // eslint-disable-next-line react/no-array-index-key
-                <li key={index}>{`${friend.first_name} ${friend.last_name}`}</li>
-              ))}
-            </ul>
-          </div>
+          <ul className="group__friends">
+            {selectedFriends.map((friend, index) => (
+              // поскольку элементы массива остаются неизменными, и порядок элементов в массиве сохраняется,
+              // то в таком случае использование индекса массива в качестве ключа является безопасным
+              // eslint-disable-next-line react/no-array-index-key
+              <li key={index}>{`${friend.first_name} ${friend.last_name}`}</li>
+            ))}
+          </ul>
         )}
       </div>
     </div>
