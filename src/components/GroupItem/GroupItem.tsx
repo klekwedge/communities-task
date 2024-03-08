20c6869212b0f98c2089b7c2392f4929403fcbf5
Andrey <antonov.andrey.10101@gmail.com>
2024-03-08 15:39:08 +0300

    feat: add pluralizeWord

--- src/components/GroupItem/GroupItem.tsx
+++ src/components/GroupItem/GroupItem.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import { Group, User } from '../../types';
 import './style.css';
 
@@ -9,6 +9,21 @@ function GroupItem({ group }: { group: Group }) {
     setSelectedFriends(selectedFriends.length ? [] : friends);
   };
 
+  const pluralizeWord = (number: number, word: string) => {
+    if (number === 1) {
+      return `${number} ${word}`;
+    }
+
+    if (number >= 2 && number <= 4) {
+      return `${number} ${word}а`;
+    }
+
+    return `${number} ${word}ов`;
+  };
+
+  const friendsCountText = useMemo(() => pluralizeWord(group.friends?.length || 0, 'друг'), [group.friends?.length]);
+  const membersCountText = useMemo(() => pluralizeWord(group.members_count, 'подписчик'), [group.members_count]);
+
   return (
     <div className="groups__group group" key={group.id}>
       <img
@@ -18,15 +33,22 @@ function GroupItem({ group }: { group: Group }) {
       />
       <div>
         <h2 className="group__name">{group.name}</h2>
-        <p>Публичность: {group.closed ? 'Закрытая' : 'Открытая'}</p>
-        <p>Подписчики: {group.members_count}</p>
-        {group.friends ? (
-          <p className="group__friends-count" onClick={() => handleFriendsClick(group.friends)}>
-            Друзья: {group.friends.length}
-          </p>
-        ) : (
-          ''
-        )}
+        <p>{group.closed ? 'Закрытая' : 'Открытая'} группа</p>
+
+        <div className="group__statistic">
+          {group.friends ? (
+            <>
+              <p className="group__friends-count" onClick={() => handleFriendsClick(group.friends)}>
+                {friendsCountText}
+              </p>
+              ·
+            </>
+          ) : (
+            ''
+          )}
+
+          <p> {membersCountText}</p>
+        </div>
 
         {selectedFriends.length > 0 && (
           <div className="group__friends">
